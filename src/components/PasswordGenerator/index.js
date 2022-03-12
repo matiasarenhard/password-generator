35434f862d9b2b69834e8159152314e1b081f3d0
Matias <matias_arenhard@hotmail.com>
2022-03-12 17:51:34 -0300

    create random password

--- src/components/PasswordGenerator/index.js
+++ src/components/PasswordGenerator/index.js
@@ -1,11 +1,54 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 import "./styles.css";
 
 const PasswordGenerator = () => { 
+    const [password, setPassword] = useState("");
     const [passwordLength, setPasswordLength] = useState(8);
     const [digitsLength, setDigitsLength] = useState(2);
     const [symbolslLength, setSymbolsLength] = useState(2);
+
+    useEffect(() => {
+        const draftPassword = [];
+
+        let lettersLength = passwordLength - digitsLength - symbolslLength;
+        if (lettersLength < 0 ) lettersLength = 0
+        
+        draftPassword.push(...Array.from({ length: digitsLength }, randomDigit));
+        draftPassword.push(...Array.from({ length: symbolslLength }, randomSymbol));
+        draftPassword.push(...Array.from({ length: lettersLength}, randomLetter));
+
+        
+        
+        setPassword(
+            draftPassword
+                .slice(0, passwordLength)
+                .sort(() => Math.random() - 0.5)
+                .join("")
+        );
+    }, [passwordLength, digitsLength, symbolslLength]);
+    
+    const randomDigit = () => { 
+        const digits = "0123456789";
+
+        return digits[Math.floor(Math.random() * digits.length)];
+    }
+
+    const randomSymbol = () => { 
+        const symbols = "#$%(+-)?{}|*";
+
+        return symbols[Math.floor(Math.random() * symbols.length)];
+    }
+
+    const randomLetter = () => { 
+        const letters = "abcdefghijklmnopqrstuvwxyz";
+
+        const letter = letters[Math.floor(Math.random() * letters.length)];
+
+        return Math.random() >= 0.5 ? letter : letter.toUpperCase();
+    }
+
+
 
     return <>
         <div className="slider">
@@ -16,15 +59,19 @@ const PasswordGenerator = () => {
 
         <div className="slider">
             <label htmlFor="digits-length">Digitos</label>
-            <input id="digits-length" type="range" min={4} max={64} value={digitsLength} onChange={({ target }) => setDigitsLength(parseInt(target.value))} />
+            <input id="digits-length" type="range" min={0} max={10} value={digitsLength} onChange={({ target }) => setDigitsLength(parseInt(target.value))} />
             <span>{digitsLength}</span>
         </div>
 
         <div className="slider">
             <label htmlFor="symbols-length">Simbolos</label>
-            <input id="symbols-length" type="range" min={4} max={64} value={symbolslLength} onChange={({ target }) => setSymbolsLength(parseInt(target.value))} />
+            <input id="symbols-length" type="range" min={0} max={10} value={symbolslLength} onChange={({ target }) => setSymbolsLength(parseInt(target.value))} />
             <span>{symbolslLength}</span>
         </div>
+
+        <h1 style={{ marginTop: "2rem", fontFamily: "monospace", textAlign: "center"}}>
+            {password}
+        </h1>
     </>;
 }
 
